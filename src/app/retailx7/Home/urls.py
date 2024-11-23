421457192f3c1df70cddf04f92e6fa40138bbcc8
Trick5t3r <snip.snap.t@gmail.com>
2024-11-23 18:17:11 +0100

    url up

--- src/app/retailx7/Home/urls.py
+++ src/app/retailx7/Home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('accounts/login/', views.user_login, name='login'),
+    path('accounts/logout/', views.user_logout, name='logout'),
     path('', views.home, name='home'),
 ]
