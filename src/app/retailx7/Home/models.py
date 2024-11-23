ea543a29d8502d3da844015a3b21df32cf7cea55
Trick5t3r <snip.snap.t@gmail.com>
2024-11-23 18:15:57 +0100

    user up

--- src/app/retailx7/Home/models.py
+++ src/app/retailx7/Home/models.py
@@ -5,6 +5,20 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)  # Champ texte long pour la bio de l'utilisateur
     images = models.ManyToManyField('Image', blank=True)  # Lien vers un modèle Image
 
+    groups = models.ManyToManyField(
+        'auth.Group', 
+        related_name='customuser_groups',  # Le related_name personnalisé
+        blank=True,
+        help_text='The groups this user belongs to.'
+    )
+    
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_permissions',  # Le related_name personnalisé
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
+
 class Image(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_images')
     image = models.ImageField(upload_to='user_images/')  # Téléchargement d'image
