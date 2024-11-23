aedad4d4a9716932caf723d9b3ab8dbefceaae24
Trick5t3r <snip.snap.t@gmail.com>
2024-11-23 19:12:40 +0100

    oubli

--- src/app/retailx7/retailx7/settings.py
+++ src/app/retailx7/retailx7/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'Home.context_processors.user_images',
+                'Home.context_processors.chatbot_context',
             ],
         },
     },
