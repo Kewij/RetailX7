097acae125d77a03d6bee72416fa7d931c32e22e
Paul Le Van Kiem <paul.levankiem@gmail.com>
2024-11-24 16:04:59 +0100

    h_hèhè

--- src/app/retailx7/Home/ia_files/pixtral_script.py
+++ src/app/retailx7/Home/ia_files/pixtral_script.py
@@ -361,4 +361,4 @@ def pipeline_reco_from_wardrobe(new_query, user, infos_text, messages):
         messages=messages,
     ).choices[0].message.content
     messages.append({"role": "assistant", "content": reco_response, "dict_infos": clothe})
-    return messages
+    return messages, clothe
