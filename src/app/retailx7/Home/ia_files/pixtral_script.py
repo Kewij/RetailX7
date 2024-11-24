637979069a481a3bdcc0b8ff9596cd9e3af73ca4
baptistebarthe <baptistebarthegold@protonmail.com>
2024-11-24 16:12:32 +0100

    a

--- src/app/retailx7/Home/ia_files/pixtral_script.py
+++ src/app/retailx7/Home/ia_files/pixtral_script.py
@@ -356,7 +356,7 @@ def pipeline_reco_from_wardrobe(new_query, user, infos_text, messages):
     wardrobe = generate_wardrobe(new_query, user)
     wardrobe = json_to_dataframe(wardrobe, key="elements")
     empty_element = generate_empty_element(new_query)
-    content = json.loads(recommend_from_wardrobe(wardrobe, empty_element))["elements"][0]
+    content = json.loads(recommend_from_wardrobe((wardrobe, empty_element)))["elements"][0]
     desc = content["description"]
     clothe = fetch_from_img_reco(content)
     reco_message = create_reco_message(clothe, desc)
