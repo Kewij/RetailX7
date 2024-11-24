75b5de400a72b518c969e60f997314feb480d92b
baptistebarthe <baptistebarthegold@protonmail.com>
2024-11-24 15:16:41 +0100

    aa

--- src/app/retailx7/Home/ia_files/pixtral_script.py
+++ src/app/retailx7/Home/ia_files/pixtral_script.py
@@ -227,7 +227,8 @@ def generate_wardrobe(new_query, user):
     ).choices[0].message.content)
     print(f"Image choisie : {img}")
     images = user.user_images.all()
-    print("Description choisie :", images[img-1].description["elements"])
+    if img != -1:
+        print("Description choisie :", images[img-1].description["elements"])
     if img == -1:
         # Wardrobe complète
         wardrobe = []
