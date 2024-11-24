a37d149cfef317f3a01eda71d97473eb4f002885
Trick5t3r <snip.snap.t@gmail.com>
2024-11-24 03:07:50 +0100

    e

---
+++ src/app/retailx7/Home/ia_files/test_generate_outift.py
@@ -0,0 +1,6 @@
+import generate_outfit_look as gn
+
+
+gn.generate_outfit_preview("Realistic photo of a young man skating, white denim sweater",
+                           "((((ugly)))), (((duplicate))), ((morbid)), ((mutilated)), [out of frame], extra fingers, mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), ((ugly)), blurry, ((bad anatomy)), (((bad proportions))), ((extra limbs)), cloned face, (((disfigured))), out of frame, ugly, extra limbs, (bad anatomy), gross proportions, (malformed limbs), ((missing arms)), ((missing legs)), (((extra arms))), (((extra legs))), mutated hands, (fused fingers), (too many fingers), (((long neck)))",
+                           r"/home/spyder/programmation/hackathon_sailes/RetailX7/src/stable_diff/test_images/pelo.jpg")
